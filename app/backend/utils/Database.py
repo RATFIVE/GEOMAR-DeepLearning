35845cdbf4648b5739dc053d0fbdd279061eca85
Marco Banzhaf <generalpegasus@web.de>
2025-02-23 00:22:03 +0100

    Füge optionale verbose-Parameter zu den Methoden upload_one und upload_many hinzu

--- app/backend/utils/Database.py
+++ app/backend/utils/Database.py
@@ -49,15 +49,17 @@ class Database():
         data = list(self.collection.find().sort(key, -1))
         return data
 
-    def upload_one(self, data):
+    def upload_one(self, data, verbose=False):
         # some database upload logic
         self.collection.insert_one(data)
-        print("Data uploaded successfully!")
+        if verbose:
+            print("Data uploaded successfully!")
     
-    def upload_many(self, data:list):
+    def upload_many(self, data:list, verbose=False):
         # some database upload logic
         self.collection.insert_many(data)
-        print("Upload Many Data successfully!")
+        if verbose:
+            print("Upload Many Data successfully!")
 
     def get_null_data(self, key):
         null_data = list(self.collection.find({key: None}))
